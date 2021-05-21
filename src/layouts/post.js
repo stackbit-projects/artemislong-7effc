5d6ec5c42b9fae43d9ae0e4ad6dbe8a156dd93b6
Stackbit Code Editor <projects@stackbit.com>
2021-05-21 22:19:12 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -19,8 +19,7 @@ export default class Post extends React.Component {
                         {_.get(this.props, 'page.frontmatter.subtitle', null) && (
                         <h3>{htmlToReact(_.get(this.props, 'page.frontmatter.subtitle', null))}</h3>
                         )}
-                        <h3 className="publish-date">{moment(_.get(this.props, 'page.frontmatter.date', null)).strftime('%A, %B %e, %Y')}</h3>
-                    </div>
+                         </div>
                 </header>
                 <div className="content">
                     {markdownify(_.get(this.props, 'page.markdown', null))}
